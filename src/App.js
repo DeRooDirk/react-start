bcec23a4456e0e8796887a9d659cdd3ddc00147a
ZooWie <55379205+DeRooDirk@users.noreply.github.com>
2021-02-15 18:50:28 +0100

    update react app
    impoted next modules

--- src/App.js
+++ src/App.js
@@ -1,71 +1,66 @@
+import React, { useState, useRef, useEffect } from "react";
+import Todolist from "./Todolist";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
-import React, { useState ,useRef ,useEffect} from 'react';
-import Todolist from './Todolist';
-import { v4 as uuidv4 } from 'uuid';
-import './App.css';
-
-
-const LOCAL_STORAGE_KEY ='todoApp.todos'
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 function App() {
-  const [ todos, setTodos ]  = useState([])
-  const todoNameRef = useRef()
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
 
-  useEffect(() =>{
-    const storedTodos =JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos)setTodos(storedTodos)
-  },[])
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
 
-  useEffect(() =>{
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
-
-  function toggleTodo(id){
-    const newTodos =[...todos]
-    const todo =newTodos.find(todo => todo.id === id)
-    todo.complete =!todo.complete
-    setTodos(newTodos)
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
   }
 
-
-  function handleAddTodo(e){
-     const name =todoNameRef.current.value
-     if(name ==='') return
-     setTodos(prevTodos =>{
-       return[...prevTodos, {id:uuidv4(),name:name,complete:false}]
-     })
-     todoNameRef.current.value = null
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
   }
 
-  function handleCLearTodos(){
-     const  newTodos = todos.filter(todo =>!todo.complete)
-     setTodos(newTodos)
+  function handleCLearTodos() {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
   }
   return (
-  <>
+    <>
+      <h1>My First React App</h1>
+      <h3>A simple Task manager</h3>
 
-  <h1>My First React App</h1>
-  <h3>A simple Task manager</h3>
+      <div className="dirk">
+        <Todolist todos={todos} toggleTodo={toggleTodo} />
 
-  <div className="dirk">
+        <input className="input" type="text" ref={todoNameRef} />
 
-  <Todolist  todos ={todos} toggleTodo={toggleTodo}/>
+        <div className="buttons">
+          <button onClick={handleAddTodo}>Add Todo</button>
+          <button onClick={handleCLearTodos}>Clear Completed Todos</button>
+        </div>
 
-  <input className="input" type="text" ref={todoNameRef}/>
-
-  <div className="buttons">
-  <button onClick ={handleAddTodo}>Add Todo</button>
-  <button onClick ={handleCLearTodos}>Clear Completed Todos</button>
-  </div>
-
-  <div> <p>{todos.filter(todo =>!todo.complete).length} Tasks Left To Do Your Leazyness </p></div> 
-
-  </div>
-
-  
-
-  </>
-  )
+        <div>          
+          <p>
+            {todos.filter((todo) => !todo.complete).length} Tasks Left To Do
+            Your Leazyness{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
