1132081443195b3f92c76c1cd000c4d20c7c11e0
ZooWie <55379205+DeRooDirk@users.noreply.github.com>
2021-02-16 19:27:51 +0100

    update app.css
    added some styling for the site dark themed

--- src/App.js
+++ src/App.js
@@ -8,58 +8,61 @@ function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storedTodos) setTodos(storedTodos);
+useEffect(() => {
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if (storedTodos) setTodos(storedTodos);
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+useEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
   }, [todos]);
 
-  function toggleTodo(id) {
-    const newTodos = [...todos];
-    const todo = newTodos.find((todo) => todo.id === id);
-    todo.complete = !todo.complete;
-    setTodos(newTodos);
+function toggleTodo(id) {
+  const newTodos = [...todos];
+  const todo = newTodos.find((todo) => todo.id === id);
+  todo.complete = !todo.complete;
+  setTodos(newTodos);
   }
 
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value;
-    if (name === "") return;
-    setTodos((prevTodos) => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
-    });
-    todoNameRef.current.value = null;
+function handleAddTodo(e) {
+  const name = todoNameRef.current.value;
+  if (name === "") return;
+  setTodos((prevTodos) => {
+  return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+  });
+  todoNameRef.current.value = null;
   }
 
-  function handleCLearTodos() {
-    const newTodos = todos.filter((todo) => !todo.complete);
-    setTodos(newTodos);
-  }
-  return (
-    <>
-      <h1>My First React App</h1>
-      <h3>A simple Task manager</h3>
+function handleCLearTodos() {
+  const newTodos = todos.filter((todo) => !todo.complete);
+  setTodos(newTodos);
+}
+return (
+  <>
+  <section className="test">
+    
+    <h3> React Task manager</h3>
 
-      <div className="dirk">
+    <div className="dirk">
+      <p >
         <Todolist todos={todos} toggleTodo={toggleTodo} />
-
-        <input className="input" type="text" ref={todoNameRef} />
-
-        <div className="buttons">
-          <button onClick={handleAddTodo}>Add Todo</button>
-          <button onClick={handleCLearTodos}>Clear Completed Todos</button>
-        </div>
-
-        <div>          
-          <p>
-            {todos.filter((todo) => !todo.complete).length} Tasks Left To Do
-            Your Leazyness{" "}
-          </p>
-        </div>
+      </p>
+      <input className="input" type="text" ref={todoNameRef} />
+        
+      <div className="buttons">
+        <button onClick={handleAddTodo}>Add Todo</button>
+        <button onClick={handleCLearTodos}>Clear Completed Todos</button>
       </div>
-    </>
+
+      <div>          
+        <p>
+            {todos.filter((todo) => !todo.complete).length} Tasks Left To Do
+            
+        </p>
+      </div>
+    </div>
+  </section>
+  </>
   );
 }
 
